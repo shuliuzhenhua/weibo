6f7b04d52a6bebf1742b39d6b675ef18babbf958
“shuliuzhenhua” <shuliuzhenhua@gmail.com>
2019-07-03 22:57:14 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,9 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
     }
 
     public function followers()
@@ -92,4 +94,5 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
 }
